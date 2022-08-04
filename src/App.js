501f26b954b48c91b20b7e44c90f07949a97980c
vonbell <divonvbell@gmail.com>
2022-08-04 15:49:09 -0400

    component setup

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import { Download, Features, SectionWrapper } from "./components";
+
 const App = () => {
     return (
         <div className="App">
             <h1 className="text-3xl font-bold underline">React Native NFT Showcase</h1>
+            <Download />
+            <Features />
+            <SectionWrapper />
         </div>
     );
 }
