035cb83a41a4518a253bc147751cc5706d6b4a28
vonbell <divonvbell@gmail.com>
2022-08-08 13:49:14 -0400

    image has cloudy logo

--- src/components/SectionWrapper.jsx
+++ src/components/SectionWrapper.jsx
@@ -6,17 +6,20 @@ import Button from './Button';
 const SectionWrapper = ({ title, description, showBtn, mockupImg, banner }) => {
     return (
         <div className={`min-h-screen ${styles.section} ${styles.bgWhite} ${banner}`}>
-            <div className={`flex items-center ${styles.boxClass}`}>
+            <div className={`flex items-center w-11/12 sm:w-full minmd:w-3/4 ${styles.boxClass}`}>
                 <div className={`${styles.descDiv} fadeLeftMini`}>
                     <h1 className={`${styles.h1Text}`}>{title}</h1>
                     <p className={`${styles.descriptionText}`}>{description}</p>
                     {showBtn && (
                         <Button
-                            assetsUrl={assets.expo}
+                            assetUrl={assets.expo}
                             link='deployed nft marketplace link'
                         />
                     )}
-                    {/* <img src={mockupImg} alt="mockup" /> */}
+                </div>
+
+                <div className={`flex-1 p-8 sm:px-0 ${styles.flexCenter}`}>
+                    <img src={mockupImg} alt="mockup" className={styles.sectionImg} />
                 </div>
             </div>
         </div>
